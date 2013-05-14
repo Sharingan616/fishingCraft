7e1fdedcc9289fe77f60abe7870d4222f034c654
Sharingan616 <sharingan616@gmail.com>
2013-05-14 14:42:39 -0700

    Fixed link to particles.png

--- common/CommonProxyFishingCraft.java
+++ common/CommonProxyFishingCraft.java
@@ -9,7 +9,7 @@ public class CommonProxyFishingCraft
 {
 //	public static String ITEMS_PNG = "/fishingCraft/textures/items.png";
 //	public static String TEX_PNG = "/fishingCraft/textures/";
-    public static String PARTS_PNG = "/fishingCraft/textures/particles.png";
+    public static String PARTS_PNG = "/fishingCraft/resources/textures/particles.png";
     public void registerRenderers()
     {
     }
