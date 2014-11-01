c80a9dfc9a5ca01d2c60e2ee27f2d5f64abc86f5
Benjamin <sharingan616@gmail.com>
2014-10-31 22:29:18 -0400

    Updated documentation for BlockBeeHive

--- common/blocks/other/BlockBeeHive.java
+++ common/blocks/other/BlockBeeHive.java
@@ -34,33 +34,12 @@ public class BlockBeeHive extends Block {
 		this.setHardness(3);
 	}
 
-	public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random)
-	{
-		for (int var6 = 0; var6 < 3; ++var6)
-		{
-			for(int i = 0; i < 10; ++i)
-			{
-				double var10000 = (double)((float)par2 + par5Random.nextFloat());
-				double var9 = (double)((float)par3 + par5Random.nextFloat());
-				var10000 = (double)((float)par4 + par5Random.nextFloat());
-				double var13 = 0.0D;
-				double var15 = 0.0D;
-				double var17 = 0.0D;
-				int var19 = par5Random.nextInt(2) * 2 - 1;
-				int var20 = par5Random.nextInt(2) * 2 - 1;
-				var13 = ((double)par5Random.nextFloat() - 0.5D) * 0.125D;
-				var15 = ((double)par5Random.nextFloat() - 0.5D) * 0.125D;
-				var17 = ((double)par5Random.nextFloat() - 0.5D) * 0.125D;
-				double var11 = (double)par4 + 0.5D + 0.25D * (double)var20;
-				var17 = (double)(par5Random.nextFloat() * 1.0F * (float)var20);
-				double var7 = (double)par2 + 0.5D + 0.25D * (double)var19;
-				var13 = (double)(par5Random.nextFloat() * 1.0F * (float)var19);
-				//TODO Add Bee particles
-			}
-		}
-	}
-
+	//TODO add Bee particles
+	
 	@SideOnly(Side.CLIENT)
+	/**
+	 * Returns different textures for different sides of the block
+	 */
 	public IIcon getIcon(int side, int par2){
 
 		if(side == 3){
